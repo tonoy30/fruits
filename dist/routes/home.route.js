f3493bb59d6443be22241e81223fba3ab20a7f03
Md. Tonoy Akando <tonoy.sust@gmail.com>
2022-10-05 16:42:34 +0600

    inital code

--- dist/routes/home.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.homeRoutes = void 0;
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-exports.homeRoutes = router;
-router.get('/', (req, res) => {
-    res.send('Hello From Home!');
-});
